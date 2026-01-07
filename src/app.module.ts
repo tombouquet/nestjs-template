d5492e57309118d8a27a15bba656adcd7566dfcf
Tom Bouquet <22462320+tombouquet@users.noreply.github.com>
2026-01-07 20:02:01 +1100

    removed bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,25 +8,11 @@ import { EmailModule } from './modules/email/email.module';
 import { loggerConfig } from './config/logger';
 import { ConfigModule } from './modules/config/config.module';
 import { typeOrmConfig } from './config/typeorm-nestjs';
-import { BullModule } from '@nestjs/bullmq';
-import { bullmqConfig } from './config/bullmq';
-import { BullBoardModule } from '@bull-board/nestjs';
-import { ExpressAdapter } from '@bull-board/express';
-import * as basicAuth from 'express-basic-auth';
 
 @Module({
   imports: [
     LoggerModule.forRoot(loggerConfig),
     TypeOrmModule.forRoot(typeOrmConfig),
-    BullModule.forRoot(bullmqConfig),
-    BullBoardModule.forRoot({
-      route: '/queues',
-      adapter: ExpressAdapter,
-      middleware: basicAuth({
-        challenge: true,
-        users: { admin: 'admin' },
-      }),
-    }),
     HealthModule,
     EmailModule,
     ConfigModule,
--- src/config/bullmq.ts
+++
@@ -1,8 +0,0 @@
-import { BullRootModuleOptions } from '@nestjs/bullmq';
-
-export const bullmqConfig: BullRootModuleOptions = {
-  connection: {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: parseInt(process.env.REDIS_PORT || '6379', 10),
-  },
-};
