f24e70cac6f98c0a96878ca4239d4149face0bde
Tom Bouquet <22462320+tombouquet@users.noreply.github.com>
2026-01-07 20:24:51 +1100

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,11 +2,7 @@ import { INestApplication } from '@nestjs/common';
 
 export const corsSetup = (app: INestApplication) => {
   const isProduction = process.env.NODE_ENV === 'production';
-  const productionOrigins = [
-    /\.cobolt\.work$/,
-    'https://creative-hub-production-frontend-894784203112.australia-southeast1.run.app',
-    'https://creative-hub-staging-frontend-894784203112.australia-southeast1.run.app',
-  ];
+  const productionOrigins = [];
 
   app.enableCors({
     origin: isProduction ? productionOrigins : ['http://localhost:3000'],
