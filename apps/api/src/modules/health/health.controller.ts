0092bb38eb081cf78504c9293a09090336d7b930
Tom Bouquet <22462320+tombouquet@users.noreply.github.com>
2026-01-21 12:50:58 +1100

    Update health.controller.ts

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
+  private readonly logger = new Logger(HealthController.name);
+
   constructor(private health: HealthCheckService) {}
 
   @Get()
   @HealthCheck()
-  check() {
-    return this.health.check([]);
+  async check() {
+    this.logger.log('Checking health');
+    const result = await this.health.check([]);
+    this.logger.log('Health check result', result);
+    return result;
   }
 }
